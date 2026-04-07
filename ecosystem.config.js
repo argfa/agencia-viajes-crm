c9261031acf288cff023d5501235cc8fd9d71958
Administrador <admin@beachcamp.com>
2026-04-07 15:07:49 -0400

    chore: configuracion de pm2 ecosystem para produccion

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: 'playa-camp-crm',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start -p 3000',
+      instances: 'max', // Utilizar todos los nucleos del CPU
+      exec_mode: 'cluster', // Modo cluster para balanceo de carga
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      }
+    }
+  ]
+};
